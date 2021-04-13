3c471b75965ae8b6f723e4f988e0adef3c328449
r83727mw <marc.wenzlawski@student.manchester.ac.uk>
2021-04-13 21:26:18 +0100

    Add Resource folder

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { TextField, Button } from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form className="resourceForm" noValidate autoComplete="off">
+          <TextField id="outlined-basic" label="Resource" variant="outlined" />
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ margin: "10px" }}
+          >
+            Search
+          </Button>
+        </form>
       </header>
     </div>
   );
